c50bcdd668e82b516a2b446bcc4abaf3c722e8dd
Paul Booth <thepaulbooth@gmail.com>
2011-03-18 16:24:35 -0400

    Added the game, basically. Tells you when you are right and wrong with haptic feedback and a large popup. Works with a dummy FlashCard. A FlashCard class is added.

--- Jabbrcube/gen/com/cube/jabbr/R.java
+++ Jabbrcube/gen/com/cube/jabbr/R.java
@@ -14,15 +14,22 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int Translation=0x7f050005;
-        public static final int addCard=0x7f050008;
-        public static final int camera=0x7f050006;
-        public static final int image=0x7f050007;
-        public static final int login=0x7f050002;
-        public static final int password=0x7f050001;
-        public static final int text01=0x7f050003;
-        public static final int translate=0x7f050004;
-        public static final int username=0x7f050000;
+        public static final int Button0=0x7f050003;
+        public static final int Button1=0x7f050004;
+        public static final int Button2=0x7f050005;
+        public static final int Button3=0x7f050006;
+        public static final int Popup=0x7f050002;
+        public static final int Title=0x7f050001;
+        public static final int Translation=0x7f05000c;
+        public static final int addCard=0x7f05000f;
+        public static final int buttonLayout=0x7f050000;
+        public static final int camera=0x7f05000d;
+        public static final int image=0x7f05000e;
+        public static final int login=0x7f050009;
+        public static final int password=0x7f050008;
+        public static final int text01=0x7f05000a;
+        public static final int translate=0x7f05000b;
+        public static final int username=0x7f050007;
     }
     public static final class layout {
         public static final int game=0x7f030000;
@@ -32,6 +39,10 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int choice1=0x7f040002;
+        public static final int choice2=0x7f040003;
+        public static final int choice3=0x7f040004;
+        public static final int choice4=0x7f040005;
         public static final int hello=0x7f040000;
     }
 }
--- Jabbrcube/src/com/cube/jabbr/Game.java
+++ Jabbrcube/src/com/cube/jabbr/Game.java
@@ -1,13 +1,99 @@
 package com.cube.jabbr;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
+import android.view.HapticFeedbackConstants;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Game extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.game);
-    }
+	/** Called when the activity is first created. */
+	Button[] choices = new Button[4];
+	String[] translatedChoices = {"yello", "zup dawg", "das transltation", "Golden Gate"};
+	TableLayout tableLayout;
+	TextView title;
+	Button popup;
+	int correctChoice = 3;
+
+	int numFlashCards = 10;
+	FlashCard[] flashcards;
+
+	Vibrator vibrator;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.game);
+		choices[0] = (Button) findViewById(R.id.Button0);
+		choices[1] = (Button) findViewById(R.id.Button1);
+		choices[2] = (Button) findViewById(R.id.Button2);
+		choices[3] = (Button) findViewById(R.id.Button3);
+		tableLayout = (TableLayout) findViewById(R.id.buttonLayout);
+		title = (TextView) findViewById(R.id.Title);
+		popup = (Button) findViewById(R.id.Popup);
+		vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+	}
+
+	public void loadCard(FlashCard flashcard) {
+		tableLayout.setBackgroundDrawable(new BitmapDrawable(flashcard.bitmap));
+		for (int i = 0; i < 4; i++) {
+			choices[i].setText(flashcard.choices[i]);
+		}
+		title.setText(flashcard.title);
+		this.correctChoice = flashcard.correctChoice;
+		popup.setVisibility(View.INVISIBLE);
+	}
+
+	public void onChoiceClicked(View view) {
+		Button button = (Button) view;
+		if (choices[correctChoice] == button) {
+			view.performHapticFeedback( HapticFeedbackConstants.VIRTUAL_KEY,
+					HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING );
+
+
+			popup.setText("Correct!\n" + title.getText().toString() + " = " + button.getText());
+			popup.setBackgroundColor(0xaa00ff00);
+			popup.setVisibility(View.VISIBLE);
+		} else {
+			Log.i("jabbrCube","Touched a choice in the game! "+button.getText().toString());
+			/*int dot = 20;      // Length of a Morse Code "dot" in milliseconds
+			int dash = 50;     // Length of a Morse Code "dash" in milliseconds
+			int short_gap = 20;    // Length of Gap Between dots/dashes
+			int medium_gap = 50;   // Length of Gap Between Letters
+			int long_gap = 100;    // Length of Gap Between Words
+			long[] pattern = {
+			    0,  // Start immediately
+			    dot, short_gap, dot, short_gap, dot,    // s
+			    medium_gap,
+			    dash, short_gap, dash, short_gap, dash, // o
+			    medium_gap,
+			    dot, short_gap, dot, short_gap, dot,    // s
+			    long_gap
+			};
+			vibrator.vibrate(pattern, -1);*/
+			button.setVisibility(View.INVISIBLE);
+			int i;
+			for ( i = 0; i < choices.length; i++) {
+				if (choices[i] == button) break;
+			}
+			popup.setText("Incorrect!\n" + button.getText().toString() + " = " + translatedChoices[i]);
+			popup.setBackgroundColor(0xaaff0000);
+			popup.setVisibility(View.VISIBLE);
+			Toast.makeText(Game.this, button.getText(), Toast.LENGTH_SHORT).show();
+			vibrator.vibrate(500);
+		}
+	}
+	
+	public void onPopupClicked(View v) {
+		popup.setVisibility(View.INVISIBLE);
+		
+		
+	}
 }
